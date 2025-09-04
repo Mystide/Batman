95b10fbdd5b9bc1ce87d879b40685acc94f049f5
Mystide <69773814+Mystide@users.noreply.github.com>
2025-09-04 17:49:13 +0200

    Update app.js

--- app.js
+++ app.js
@@ -14,14 +14,17 @@ const el = {
   mResetFilters:$('#mResetFilters'), clearSearch:$('#clearSearch')
 };
 
-const collator = new Intl.Collator('de', {numeric:true, sensitivity:'base'});
+const collator = new Intl.Collator('de', { numeric: true, sensitivity: 'base' });
 
-const SERVER_URL = (typeof process !== 'undefined' && process.env.SERVER_URL) ||
-  config.SERVER_URL || 'https://meinserver.example:3000';
+const SERVER_URL =
+  (typeof process !== 'undefined' && process.env.SERVER_URL) ||
+  config.SERVER_URL ||
+  'https://meinserver.example:3000';
 
 init();
-async function init(){
-    try {
+
+async function init() {
+  try {
     const res = await fetch(`${SERVER_URL}/read-status`);
     if (res.ok) {
       const list = await res.json();
