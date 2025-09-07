10d2adf15f35c7bb9791d55be51ebf859d43b22b
Mystide <69773814+Mystide@users.noreply.github.com>
2025-09-07 16:15:31 +0200

    Create constants.js

---
+++ constants.js
@@ -0,0 +1,4 @@
+export const GIST_ID = 'f4ac4f63f8f150bde113a52246bdea28';
+export const FILE = 'readStatus.json';
+export const LS_KEY = 'comic-tracker-read';
+export const VIEW_KEY = 'comic-tracker-view';
