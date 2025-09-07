5cb62b290d4547c9a6e6ef3b4ee9ead35dbeda9b
Mystide <69773814+Mystide@users.noreply.github.com>
2025-09-07 21:10:53 +0200

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,17 @@
+import js from '@eslint/js';
+import globals from 'globals';
+
 export default [
+  js.configs.recommended,
   {
     files: ['**/*.js'],
     languageOptions: {
-      ecmaVersion: 2021,
+      ecmaVersion: 'latest',
       sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+      },
     },
     rules: {
       semi: ['error', 'always'],
