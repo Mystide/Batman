227e6d47db3f82175a8d6585260b5ef5015c1315
Mystide <69773814+Mystide@users.noreply.github.com>
2025-09-07 21:16:36 +0200

    Delete tests/render.test.js

--- tests/render.test.js
+++
@@ -1,5 +0,0 @@
-// This test relies on a DOM environment which is not available in the current setup.
-// Skipping to ensure the rest of the suite can run under the Node test environment.
-describe.skip('render', () => {
-  test('creates cards with title and toggle', () => {});
-});
