6fe6c085154e6768913c683d0abc391d9c1e3ef4
Mystide <69773814+Mystide@users.noreply.github.com>
2025-09-09 22:34:24 +0200

    Create demo-data.js

---
+++ src/demo-data.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    id: 'batman_year_one',
+    title: 'Batman 1',
+    issue: '1',
+    year: 1987,
+    series: 'Batman (1987)',
+    cover: 'https://upload.wikimedia.org/wikipedia/en/3/3d/Batman_404.png',
+    event: 'Year One',
+    date: '1987-02-01',
+  },
+  {
+    id: 'killing_joke',
+    title: 'Batman 10',
+    issue: '10',
+    year: 1988,
+    series: 'One-Shot (1988)',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/9/9b/Batman_the_killing_joke.jpg',
+    event: 'Killing Joke',
+    date: '1988-03-29',
+  },
+  {
+    id: 'hush_608',
+    title: 'Batman 100',
+    issue: '100',
+    year: 2002,
+    series: 'Batman (2002)',
+    cover:
+      'https://upload.wikimedia.org/wikipedia/en/9/9f/Batman_Hush_cover.jpg',
+    event: 'Hush',
+    date: '2002-10-01',
+  },
+];
